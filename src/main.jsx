3dc501f813a289a292895439a635fc18f40a8e82
Anurag Zete <anuragzete27@outlook.com>
2025-07-26 14:46:53 +0530

    Added: Maintenance page
    Added maintenance page till the site is completely ready to use.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 import './index.css';
+import Maintenance from "./services/Maintenance.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      {/*<App />*/}
+        <Maintenance />
     </ThemeProvider>
   </StrictMode>
 );
